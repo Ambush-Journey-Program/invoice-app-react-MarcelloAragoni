c3cda7af4b749593f23bb46078e908cbed789f36
Marcello <marcello_gp_@hotmail.com>
2022-12-05 09:39:05 -0300

    last changes made on pair programming

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,30 +3,28 @@ import { sortParams } from '../../utilities/consts';
 import { getInvoices } from '../../services/invoice';
 import SearchInput from '../../components/search-input';
 
-const urlParams = new URLSearchParams({
-  ...sortParams
-});
-
-export async function handleSubmit(event) {
-  event.preventDefault();
-
-  const [clientNameEl] = event.target;
-
-  const filterParams = [{ name: 'clientName', value: clientNameEl.value }];
-
-  filterParams.forEach((filter) => {
-    if (filter.value) {
-      urlParams.set(filter.name, filter.value);
-    } else {
-      urlParams.delete(filter.name);
-    }
-  });
-
-  await getInvoices(urlParams);
-}
-
 export default function Home() {
   const [searchValue, setSearchValue] = useState('');
+
+  const urlParams = new URLSearchParams({
+    ...sortParams
+  });
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const filterParams = [{ name: 'clientName', value: searchValue }];
+
+    filterParams.forEach((filter) => {
+      if (filter.value) {
+        urlParams.set(filter.name, filter.value);
+      } else {
+        urlParams.delete(filter.name);
+      }
+    });
+
+    await getInvoices(urlParams);
+  }
 
   return (
     <form onSubmit={handleSubmit}>
