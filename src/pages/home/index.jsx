b7f9b0a92ce2b9f14f423e7ae58163805ee4adbf
Marcello <32502447+MarcelloAragoni@users.noreply.github.com>
2022-12-05 09:51:13 -0300

    Update index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,36 +2,10 @@ import { useState } from 'react';
 import { sortParams } from '../../utilities/consts';
 import { getInvoices } from '../../services/invoice';
 import SearchInput from '../../components/search-input';
-<<<<<<< HEAD
-=======
-
-const urlParams = new URLSearchParams({
-  ...sortParams
-});
-
-export async function handleSubmit(event) {
-  event.preventDefault();
-
-  const [clientNameEl] = event.target;
-
-  const filterParams = [{ name: 'clientName', value: clientNameEl.value }];
-
-  filterParams.forEach((filter) => {
-    if (filter.value) {
-      urlParams.set(filter.name, filter.value);
-    } else {
-      urlParams.delete(filter.name);
-    }
-  });
-
-  await getInvoices(urlParams);
-}
->>>>>>> origin/main
 
 export default function Home() {
   const [searchValue, setSearchValue] = useState('');
 
-<<<<<<< HEAD
   const urlParams = new URLSearchParams({
     ...sortParams
   });
@@ -52,8 +26,6 @@ export default function Home() {
     await getInvoices(urlParams);
   }
 
-=======
->>>>>>> origin/main
   return (
     <form onSubmit={handleSubmit}>
       <SearchInput onChange={setSearchValue} value={searchValue} />
