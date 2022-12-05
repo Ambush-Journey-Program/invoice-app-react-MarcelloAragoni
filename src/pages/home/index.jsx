d0d8c3e8198dc0f89eac55cc1c0f601309e8d6f3
Marcello <32502447+MarcelloAragoni@users.noreply.github.com>
2022-12-05 13:56:39 -0300

    create status search filter feature (#5)
    ## invoice-app-React-MarcelloAragoni
    
    ## Description
    create status search filter feature
    
    ## PrintScreen
    <img width="517" alt="image" src="https://user-images.githubusercontent.com/32502447/205664155-438054b7-e82e-4e48-87fa-175c2ede9933.png">

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import { sortParams } from '../../utilities/consts';
 import { getInvoices } from '../../services/invoice';
 import SearchInput from '../../components/search-input';
+import SearchStatus from '../../components/search-status';
 
 export default function Home() {
   const [searchValue, setSearchValue] = useState('');
+  const [searchStatus, setSearchStatus] = useState('');
 
   const urlParams = new URLSearchParams({
     ...sortParams
@@ -13,7 +15,11 @@ export default function Home() {
   async function handleSubmit(event) {
     event.preventDefault();
 
-    const filterParams = [{ name: 'clientName', value: searchValue }];
+    const filterParams = [
+      { name: 'clientName', value: searchValue },
+
+      { name: 'status', value: searchStatus }
+    ];
 
     filterParams.forEach((filter) => {
       if (filter.value) {
@@ -29,6 +35,7 @@ export default function Home() {
   return (
     <form onSubmit={handleSubmit}>
       <SearchInput onChange={setSearchValue} value={searchValue} />
+      <SearchStatus onChange={setSearchStatus} statusValue={searchStatus} />
     </form>
   );
 }
