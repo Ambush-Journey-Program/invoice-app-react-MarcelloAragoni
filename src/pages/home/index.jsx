5c21d2b0f8bde0b6a84cf6ab6020be65685667e0
Marcello <32502447+MarcelloAragoni@users.noreply.github.com>
2022-12-05 09:48:37 -0300

    Feature input (#4)
    ## invoice-app-React-MarcelloAragoni
    
    ## Description
    changes made on pair programming 
    
    ## PrintScreen
    <img width="1107" alt="image" src="https://user-images.githubusercontent.com/32502447/205138234-160d5610-4476-45ca-a559-bc5ce314aec1.png">

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { sortParams } from '../../utilities/consts';
 import { getInvoices } from '../../services/invoice';
 import SearchInput from '../../components/search-input';
+<<<<<<< HEAD
+=======
 
 const urlParams = new URLSearchParams({
   ...sortParams
@@ -24,10 +26,34 @@ export async function handleSubmit(event) {
 
   await getInvoices(urlParams);
 }
+>>>>>>> origin/main
 
 export default function Home() {
   const [searchValue, setSearchValue] = useState('');
 
+<<<<<<< HEAD
+  const urlParams = new URLSearchParams({
+    ...sortParams
+  });
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const filterParams = [{ name: 'clientName', value: searchValue }];
+
+    filterParams.forEach((filter) => {
+      if (filter.value) {
+        urlParams.set(filter.name, filter.value);
+      } else {
+        urlParams.delete(filter.name);
+      }
+    });
+
+    await getInvoices(urlParams);
+  }
+
+=======
+>>>>>>> origin/main
   return (
     <form onSubmit={handleSubmit}>
       <SearchInput onChange={setSearchValue} value={searchValue} />
