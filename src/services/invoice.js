6feb877b1eb0ef062e1c8025b25d31a4b24cd24f
Marcello <32502447+MarcelloAragoni@users.noreply.github.com>
2022-12-06 15:27:54 -0300

    refactoring search bar (#7)
    ## invoice-app-react-MarcelloAragoni
    
    ## Description
    refactoring of the code
    
    ## PrintScreen
    <img width="1127" alt="image" src="https://user-images.githubusercontent.com/32502447/205988341-1a0ce791-befe-4336-954c-127b4dc1d173.png">

--- src/services/invoice.js
+++ src/services/invoice.js
@@ -2,5 +2,5 @@ import { BASE_URL } from '../utilities/consts';
 
 export async function getInvoices(params) {
   const response = await fetch(`${BASE_URL}?${params}`);
-  return response.json();
+  return await response.json();
 }
